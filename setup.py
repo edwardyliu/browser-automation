43989eea99936ef35f3c42c8cefbf8d551e057ad
Edward Y. Liu <edwardy.liu@mail.utoronto.ca>
2020-04-13 17:41:27 -0400

    add setup

--- setup.py
+++ setup.py
@@ -31,8 +31,37 @@ def shell(cmd:str, show:bool=True)->str:
     return stdout
 
 # == Main Function ==
-def main():
-    print("main")
+def main(env:str="linux"):
+    print("========================================================")
+    print("=== Make Folder(s): ")
+    print("========================================================")
+    resources = os.path.join(ROOTPATH, "app/resources/")
+    logs = os.path.join(ROOTPATH, "app/logs/")
+    print(resources)
+    if not os.path.exists(resources):
+        shell(f"mkdir -p {resources}")
+    print(logs)
+    if not os.path.exists(logs):
+        shell(f"mkdir -p {logs}")
+
+    print("========================================================")
+    print("=== Download & Install Geckodriver: ")
+    print("========================================================")
+    version = "v0.26.0"
+    env = env.lower()
+    if env == "windows":
+        name = "geckodriver-"+version+"-win64.zip"
+        command = f"curl -L -O https://github.com/mozilla/geckodriver/releases/download/{version}/{name}"
+    elif env == "darwin":
+        name = "geckodriver-"+version+"-macos.tar.gz"
+        command = f"curl -L -O https://github.com/mozilla/geckodriver/releases/download/{version}/{name}"
+    else:
+        name = "geckodriver-"+version+"-linux64.tar.gz"
+        command = f"curl -L -O https://github.com/mozilla/geckodriver/releases/download/{version}/{name}"
+    shell("cd {0} && {1}".format(resources, command))
+
+    stdout = shell(f"cd {resources} && tar -xvzf {name}", show=False)
+    shell(f"chmod +x {os.path.join(resources, stdout)}")
 
 if __name__ == "__main__":
     main()
