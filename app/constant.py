13cc277f8307d7e70c863e1478f4a4c5195498aa
Edward Y. Liu <edwardy.liu@mail.utoronto.ca>
2020-04-15 18:32:52 -0400

    rename fsm folder to "jsons"; add path constant

--- app/constant.py
+++ app/constant.py
@@ -1,4 +1,11 @@
+# == Import(s) ==
+import os
+from pathlib import Path
+
 # == Constant(s) ==
+# => Parser
+JSON_DIRPATH=os.path.join(Path(__file__).parents[0], "jsons/")
+
 # => Machine
 IDLE=2.
 CLICK=1.
