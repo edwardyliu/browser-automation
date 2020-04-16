2bd955730d9b0d32c46442577966a88e46625ae9
Edward Y. Liu <edwardy.liu@mail.utoronto.ca>
2020-04-16 16:40:00 -0400

    mod DCG model

--- app/model.py
+++ app/model.py
@@ -2,31 +2,14 @@
 # => System
 from dataclasses import dataclass
 
-# => External
-import transitions
-
 # == Data Model(s) ==
-@dataclass(frozen=True)
-class Method:
-    """Define a method/function execution strategy:
-
-    Execute a method/function given a keyword <action> and a list of string parameters <arguments>
-    """
-    action: str
-    arguments: [str]
-
 @dataclass(frozen=True)
 class DirectedCycleGraph:
     """Define a Directed Cycle Graph
 
-    A finite state machine can be constructed using:
-        1. nodes: the states
-        2. connections: the transitions
-        3. methods: the actions
+    A finite state machine cycle is constructed with <nodes>: 
+        i.e. 1=>2=>...=>N=>1
     """
     name: str
-    environment: str
-
-    nodes: [transitions.State]
-    connections: [dict]
-    methods: [Method]
+    env: str
+    nodes: list
