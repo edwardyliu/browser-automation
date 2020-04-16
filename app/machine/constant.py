fb4c9f2d05b369a1bd0a069e2b3ff79ba4ca34b4
Edward Y. Liu <edwardy.liu@mail.utoronto.ca>
2020-04-16 16:39:23 -0400

    make a separate machine package

---
+++ app/machine/constant.py
@@ -0,0 +1,9 @@
+# == Import(s) ==
+
+# == Constant(s) ==
+# => Machine
+TIMEOUT=10.
+
+CLICK=1.
+GET=1.
+IDLE=2.
---
+++ app/machine/utils.py
@@ -0,0 +1,39 @@
+# == Import(s) ==
+import datetime
+import logging
+import pytz
+
+# == Utility Function(s) ==
+def timezone_converter_est(*args):
+    """A timezone converter that converts from UTC to EST
+    """
+    utc_datetime = pytz.utc.localize(datetime.datetime.utcnow())
+    est_timezone = pytz.timezone("US/Eastern")
+    converted = utc_datetime.astimezone(est_timezone)
+    return converted.timetuple()
+
+def get_logger(uid:str)->logging.Logger:
+    """Get logging object
+
+    Parameters
+    ----------
+    uid: str
+        The logger's ID
+
+    Returns
+    -------
+    Logger:
+        The logging object
+    """
+    log = logging.getLogger(uid)
+    log.setLevel(logging.INFO)
+
+    fmt = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+    fmt.converter = timezone_converter_est
+
+    handle = logging.StreamHandler()
+    handle.setLevel(logging.INFO)
+    handle.setFormatter(fmt)
+
+    log.addHandler(handle)
+    return log
