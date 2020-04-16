abf9248e912bfc276866198dc680f0958a7158b4
Edward Y. Liu <edwardy.liu@mail.utoronto.ca>
2020-04-16 16:39:39 -0400

    adjust and add commenting to parser

--- app/parser.py
+++ app/parser.py
@@ -2,6 +2,7 @@
 # => Local
 from . import constant
 from . import model
+from . import machine
 
 # => System
 import os
@@ -11,39 +12,46 @@ from pathlib import Path
 # => External
 import transitions
 
-def get()->[model.DirectedCycleGraph]:
-    dcgs = [model.DirectedCycleGraph]
+def get_dcgs()->list:
+    """Get a list of Directed Cycle Graph models via files retrieved from '<basedir>/app/json/.../*.json'
+
+    Returns
+    -------
+    list:
+        A list of Directed Cycle Graph models
+    """
+    dcgs = []
     for json_file in list( Path(constant.JSON_DIRPATH).rglob("*.[jJ][sS][oO][nN]") ):
-        dcgs.append( parse(json_file) )
+        dcg = parse_json(json_file)
+        dcgs.append(dcg)
     return dcgs
 
-def parse(json_file)->model.DirectedCycleGraph:
-    
+def parse_json(json_file)->model.DirectedCycleGraph:
+    """Parse a JSON file into a Directed Cycle Graph model
+
+    Parameters
+    ----------
+    json_file: str
+        The JSON file path
+
+    Returns
+    -------
+    model.DirectedCycleGraph:
+        The model object
+    """
     try:
         with open(json_file) as fptr:
             raw = json.load(fptr)
 
-        nodes = [transitions.State]
-        connections = [dict]
-        methods = [model.Method]
+        nodes = []
         for node in raw["graph"]:
-            nodes.append(transitions.State(name=node[1]))
-            connections.append({
-                "trigger": node[0].lower(), 
-                "source": node[1], 
-                "dest": node[2]
-            })
             try:
-                arguments = node[3]
+                arguments:[str] = node[3]
             except IndexError:
                 arguments = [str]
-            methods.append( model.Method(node[0], arguments) )
+            nodes.append( machine.model.Action(name=node[1], key=node[0].lower(), arguments=arguments) )
 
-        return model.DirectedCycleGraph(raw["name"], raw["environment"], nodes, connections, methods)
+        return model.DirectedCycleGraph(name=raw["name"], env=raw["environment"], nodes=nodes)
     
     except Exception:
         raise ValueError(f"Parser.parse | Invalid JSON File: {json_file}")
-
-if __name__ == "__main__":
-    dcgs = get()
-    print( dcgs )
