cfd05b2e585c1803c994501013b57fefeebfa6c0
Edward Y. Liu <edwardy.liu@mail.utoronto.ca>
2020-04-25 22:52:11 -0400

    add testing for models

---
+++ app/backend/webauto/tests/testModels.py
@@ -0,0 +1,48 @@
+# == Import(s) ==
+# => Local
+from .. import models
+from .. import utils
+
+# => System
+import unittest
+
+class TestModels(unittest.TestCase):
+    
+    def test_command_model(self):
+        command = models.Command(
+            label="printf", 
+            target="A beautiful mind ${name}",
+            argv=["Edward"])
+        self.assertEqual(command.label, "printf")
+        self.assertEqual(command.target, "A beautiful mind ${name}")
+        self.assertEqual(command.argv, ["Edward"])
+
+        command.argv.append("Beau")
+        self.assertEqual(command.argv, ["Edward", "Beau"])
+        self.assertEqual(command.argv.pop(), "Beau")
+        self.assertEqual(command.argv, ["Edward"])
+
+    def test_sequence_model(self):
+        cmda = models.Command(label="printf", target="A beautiful mind ${name}", argv=["Edward"])
+        cmdb = models.Command(label="printf", target="An amazing experience with ${name}", argv=["Beau"])
+        cmdc = models.Command(label="printf", target="A ${name} cause", argv=["noble"])
+        sequence = models.Sequence(
+            name="A Web Sequence",
+            env="DEV",
+            cmds=[cmda, cmdb])
+        self.assertEqual(sequence.name, "A Web Sequence")
+        self.assertEqual(sequence.env, "DEV")
+        self.assertEqual(sequence.cmds, [cmda, cmdb])
+
+        sequence.cmds.append(cmdc)
+        self.assertEqual(sequence.cmds, [cmda, cmdb, cmdc])
+        self.assertEqual(sequence.cmds.pop(), cmdc)
+        self.assertEqual(sequence.cmds, [cmda, cmdb])
+
+    def test_worker_model(self):
+        driver = utils.get_webdriver()
+        worker = models.Worker("A Worker", driver)
+        driver.quit()
+
+if __name__ == "__main__":
+    unittest.main()
